31b51f8e50c330f66851bf5faa3c167a84f52ea7
Hossam Hassan Sakr <HossamHSakr@users.noreply.github.com>
2019-07-07 17:49:48 +0200

    Update Run.bat

--- HotFix/ALCVirusCorrections/Run.bat
+++ HotFix/ALCVirusCorrections/Run.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\Autodesk\3ds Max 2016\3dsmax.exe" -q -silent -mip -U MAXScript "\\manager\Hive_server_Elements\Scripts\HossamSakr_Tools\ALCVirusCorrections\ALC_CRP_Fixup_v1_2_CheckingWithTxt.ms" "\\Server\Hive_Server_01\Projects\ALJ-After Sales Campagin\00_Assets\01_Characters\03_Final\Cars\07_Toyota_RAV4\Rav4_Black_Jawhra\Toyota_RAV4_V5_Black.max"
+"C:\Program Files\Autodesk\3ds Max 2016\3dsmax.exe" -q -silent -mip -U MAXScript "\\Scripts\HossamSakr_Tools\ALCVirusCorrections\ALC_CRP_Fixup_v1_2_CheckingWithTxt.ms" "\\Server\MaxFilePath.max"
